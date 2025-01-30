eb84290a2365ccba27fb2a125322a0fa03d020b1
Vania Rheta LF <vaniarhetalf@gmail.com>
2025-01-30 11:10:26 +0700

    Update css dan html 2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 // Ini File Javascript
+
+
+// Kalkulasi hasil perhitungan berat badan/tinggi badan
+function kalkulasi() {
+    let beratBadan = document.getElementById("in-berat-badan").value;
+    let usia = document.getElementById("in-usia").value;
+    let tinggiBadan = document.getElementById("in-tinggi-badan").value;
+
+    let hasil = beratBadan/((tinggiBadan/100)^2);
+
+    document.write('Hasilnya adalah' + hasil)
